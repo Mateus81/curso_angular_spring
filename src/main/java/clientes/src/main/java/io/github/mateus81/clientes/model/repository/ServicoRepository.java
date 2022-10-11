73c8ea567d8a09e67259a4514e78a4a48bb8b212
Mateus81 <msnmateus@yahoo.com.br>
2022-10-11 12:22:31 -0300

    Controller

--- src/main/java/clientes/src/main/java/io/github/mateus81/clientes/model/repository/ServicoRepository.java
+++
@@ -1,9 +0,0 @@
-package clientes.src.main.java.io.github.mateus81.clientes.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import clientes.src.main.java.io.github.mateus81.clientes.model.entity.Servico;
-
-public interface ServicoRepository extends JpaRepository<Servico, Integer> {
-
-}
